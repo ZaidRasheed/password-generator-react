470295c129586f4e262d324c63a02f78542004c2
ZaidRasheed <94339310+ZaidRasheed@users.noreply.github.com>
2022-08-12 00:27:18 -0400

    strict mode added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
